538c0c017e09d9bc900167f0b4679d50423c4ad2
Eddie Chung <designered@gmail.com>
2021-11-17 11:13:54 +0100

    setup next, get data from contentful

---
+++ pages/index.js
@@ -0,0 +1,36 @@
+import { createClient } from "contentful";
+import RecipeCard from "../components/RecipeCard";
+
+export const getStaticProps = async () => {
+  const client = createClient({
+    space: process.env.CONTENTFUL_SAPCE_ID,
+    accessToken: process.env.CONTENTFUL_ACCESS_TOKEN,
+  });
+
+  const res = await client.getEntries({ content_type: "recipe" });
+
+  return {
+    props: {
+      recipes: res.items,
+    },
+  };
+};
+
+export default function Recipes({ recipes }) {
+  console.log(recipes);
+  return (
+    <div className="recipe-list">
+      {recipes.map((recipe) => (
+        <RecipeCard recipe={recipe} key={recipe.fields.id} />
+      ))}
+
+      <style jsx>{`
+        .recipe-list {
+          display: grid;
+          grid-template-columns: 1fr 1fr;
+          grid-gap: 20px 60px;
+        }
+      `}</style>
+    </div>
+  );
+}
